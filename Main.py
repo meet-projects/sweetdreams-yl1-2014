6a422a2f9eb0f59f0614e13524d9aa759da97601
Moshe Kanooni <moshe13@meet.mit.edu>
2014-03-24 20:02:27 +0200

    label classes

--- Main.py
+++ Main.py
@@ -1,18 +1,30 @@
 import pygame as pg
 import imagesclass
+import buttonclass
+import labels
+
+
 if __name__=="__main__":
-	pg.init()
-	main_screen = pg.display.set_mode((400, 400))
-	main_screen.fill((255,255,255))
-	image1=imagesclass.image(100, 100, "pygame_tiny.png")
-	image1.draw_image(main_screen)
-	while True: 
-        	ev = pg.event.poll()
-        	if ev.type == pg.QUIT: 
-            		sys.exit()
-        	if ev.type == pg.MOUSEBUTTONDOWN:
-            		x, y = ev.pos
-           		if image1.image_surface.collidepoint(x, y):
-				image1.clear_images(main_screen)
-		pg.display.flip()
+    pg.init()
+    main_screen = pg.display.set_mode((600, 600))
+    main_screen.fill((255,255,255))
+    image1=imagesclass.image(100, 100, "pygame_tiny.png")
+    image1.draw_image(main_screen)
+    button1=buttonclass.button(300, 100, 50, 50, "Butsdasdfsdfton1", 1, (255, 0, 0))
+    button1.draw_button(main_screen)
+    label1 = labels.label((255,0,0), 200, 200, 100, 300, "label", 25)
+    label1.draw_label(main_screen)
+
+    while True: 
+        ev = pg.event.poll()
+        if ev.type == pg.QUIT: 
+                sys.exit()
+        if ev.type == pg.MOUSEBUTTONDOWN:
+            x, y = ev.pos
+            print ev.pos
+            if image1.image_surface.collidepoint(x, y):
+                image1.clear_images(main_screen)    
+            if button1.button_rec.collidepoint(x, y):
+                button1.clear_button(main_screen)
+        pg.display.flip()
  
--- labels.py
+++ labels.py
@@ -1,10 +1,23 @@
-class Label(object):
-	def __init__(self, label_color, label_size_x, label_size_y, label_location_x, label_location_y, label_font):
-		self.Surface = 	pygame.Surface([label_size_x, label_size_y])	
-		self.rectangle = pygame.Rect(label_location_x, label_location_y, label_size_x, label_size_y)
-		self.color = label_color
-		self.size = label.size
-		self.location = label_location
-		self.font = location_font
-	def draw_label(self):
+import pygame as pg
+class label(object):
+	def __init__(self, label_color, label_size_x, label_size_y, label_location_x, label_location_y, label_text, label_fontsize):
+		self.colorl = label_color
+		self.sizex = label_size_x
+		self.sizey = label_size_y
+		self.locationx = label_location_x
+		self.locationy = label_location_y
+		self.fontsize = label_fontsize
+		self.text = label_text
+	def draw_label(self, place):
+		self.label_rec = pg.Rect(self.locationx, self.locationy, self.sizex, self.sizey)
+		self.label = pg.font.Font(None, label_fontsize)
+		self.label = self.label.render(self.text, 1, self.colorl, (255, 255, 255))
+		main_screen.blit(self.label, self.label_rec)
+	def clear_label(self, place):
+		clear_label_surface = pg.Surface([self.sizex,self.sizey])
+		clear_label_surface.fill((255,255,255))
+		place.blit(self.label_surface, self.label_rec)
+
+
+
 
