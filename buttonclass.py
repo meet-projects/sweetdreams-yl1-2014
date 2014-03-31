2cd5b5d0f98b6ba973501b9619423c1a868de371
Yoni Gootkin <yoni13@meet.mit.edu>
2014-03-31 20:02:09 +0300

    almost done babygit add --all

--- buttonclass.py
+++ buttonclass.py
@@ -1,21 +1,22 @@
 import pygame as pg
 class button(object):
-	def __init__(self, xlocation_button, ylocation_button, widht_button, height_button, text_button, onclickaction, color):
-        	self.xlocb = xlocation_button
-        	self.ylocb = ylocation_button
-        	self.widthb = widht_button
-        	self.heightb = height_button
+	def __init__(self, xlocation_button, ylocation_button, widht_button, height_button, text_button, text_size, color):
+		self.xlocb = xlocation_button
+		self.ylocb = ylocation_button
+		self.widthb = widht_button
+		self.heightb = height_button
 		self.textb = text_button
-		self.action = onclickaction
+		self.txtsize = text_size
 		self.colorb = color
+
 	
 	def draw_button(self, place):
 		self.button_rec = pg.Rect(self.xlocb, self.ylocb,self.widthb, self.heightb)
 		self.button_surface = pg.Surface([self.widthb,self.heightb])
 		self.button_surface.fill(self.colorb)		
 		place.blit(self.button_surface, self.button_rec)
-		buttonlabel = pg.font.Font(None, 30)
-		self.buttonlabel = buttonlabel.render(self.textb, 1, (0, 0, 0))
+		buttonlabel = pg.font.Font(None, self.txtsize)
+		self.buttonlabel = buttonlabel.render(self.textb, 1, self.colorb, (0, 0, 0))
 		place.blit(self.buttonlabel, self.button_rec)
 		
 	def clear_button(self, place):
--- labels.py
+++ labels.py
@@ -10,9 +10,9 @@ class label(object):
 		self.text = label_text
 	def draw_label(self, place):
 		self.label_rec = pg.Rect(self.locationx, self.locationy, self.sizex, self.sizey)
-		self.label = pg.font.Font(None, label_fontsize)
+		self.label = pg.font.Font(None, self.fontsize)
 		self.label = self.label.render(self.text, 1, self.colorl, (255, 255, 255))
-		main_screen.blit(self.label, self.label_rec)
+		place.blit(self.label, self.label_rec)
 	def clear_label(self, place):
 		clear_label_surface = pg.Surface([self.sizex,self.sizey])
 		clear_label_surface.fill((255,255,255))
